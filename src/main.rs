094c60ca9273ee567336699d57046692ce2ea3ac
gabrielabezerra <gabrieladecarvalhobezerra@gmail>
2025-12-03 20:10:03 -0300

    day 3

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,26 @@
 mod day1;
 mod day2;
+mod day3;
 
 fn main() { 
-    println!("\n=== DAY 1 - part 1 ===");
-    day1::part1(day1::TEST_INPUT);
-    day1::part1(day1::INPUT);
-    println!("\n=== DAY 1 - part 2 ===");
-    day1::part2(day1::TEST_INPUT);
-    day1::part2(day1::INPUT);
+   println!("\n=== DAY 1 - part 1 ===");
+   day1::part1(day1::TEST_INPUT);
+   day1::part1(day1::INPUT);
+   println!("\n=== DAY 1 - part 2 ===");
+   day1::part2(day1::TEST_INPUT);
+   day1::part2(day1::INPUT);
 
-    println!("\n=== DAY 2 - part 1 ===");
-    day2::part1(day2::TEST_INPUT);
-    day2::part1(day2::INPUT);
-    println!("\n=== DAY 2 - part 2 ===");
-    day2::part2(day2::TEST_INPUT);
-    day2::part2(day2::INPUT);
+   println!("\n=== DAY 2 - part 1 ===");
+   day2::part1(day2::TEST_INPUT);
+   day2::part1(day2::INPUT);
+   println!("\n=== DAY 2 - part 2 ===");
+   day2::part2(day2::TEST_INPUT);
+   day2::part2(day2::INPUT);
+
+   println!("\n=== DAY 3 - part 1 ===");
+   day3::part1(day3::TEST_INPUT);
+   day3::part1(day3::INPUT);
+   println!("\n=== DAY 3 - part 2 ===");
+   day3::part2(day3::TEST_INPUT);
+   day3::part2(day3::INPUT);
 }
