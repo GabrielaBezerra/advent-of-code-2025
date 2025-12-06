a00e3bc71b170b0280f280979fc6cb252823c39d
gabrielabezerra <gabrieladecarvalhobezerra@gmail>
2025-12-05 22:29:28 -0300

    day 5 part 1 + wip part 2

--- src/main.rs
+++ src/main.rs
@@ -2,33 +2,41 @@ mod day1;
 mod day2;
 mod day3;
 mod day4;
+mod day5;
 
 fn main() { 
-   println!("\n=== DAY 1 - part 1 ===");
-   day1::part1(day1::TEST_INPUT);
-   day1::part1(day1::INPUT);
-   println!("\n=== DAY 1 - part 2 ===");
-   day1::part2(day1::TEST_INPUT);
-   day1::part2(day1::INPUT);
+   // println!("\n=== DAY 1 - part 1 ===");
+   // day1::part1(day1::TEST_INPUT);
+   // day1::part1(day1::INPUT);
+   // println!("\n=== DAY 1 - part 2 ===");
+   // day1::part2(day1::TEST_INPUT);
+   // day1::part2(day1::INPUT);
+   //
+   // println!("\n=== DAY 2 - part 1 ===");
+   // day2::part1(day2::TEST_INPUT);
+   // day2::part1(day2::INPUT);
+   // println!("\n=== DAY 2 - part 2 ===");
+   // day2::part2(day2::TEST_INPUT);
+   // day2::part2(day2::INPUT);
+   //
+   // println!("\n=== DAY 3 - part 1 ===");
+   // day3::part1(day3::TEST_INPUT);
+   // day3::part1(day3::INPUT);
+   // println!("\n=== DAY 3 - part 2 ===");
+   // day3::part2(day3::TEST_INPUT);
+   // day3::part2(day3::INPUT);
+   //
+   // println!("\n=== DAY 4 - part 1 ===");
+   // day4::part1(day4::TEST_INPUT);
+   // day4::part1(day4::INPUT);
+   // println!("\n=== DAY 4 - part 2 ===");
+   // day4::part2(day4::TEST_INPUT);
+   // day4::part2(day4::INPUT);
 
-   println!("\n=== DAY 2 - part 1 ===");
-   day2::part1(day2::TEST_INPUT);
-   day2::part1(day2::INPUT);
-   println!("\n=== DAY 2 - part 2 ===");
-   day2::part2(day2::TEST_INPUT);
-   day2::part2(day2::INPUT);
-
-   println!("\n=== DAY 3 - part 1 ===");
-   day3::part1(day3::TEST_INPUT);
-   day3::part1(day3::INPUT);
-   println!("\n=== DAY 3 - part 2 ===");
-   day3::part2(day3::TEST_INPUT);
-   day3::part2(day3::INPUT);
-
-   println!("\n=== DAY 4 - part 1 ===");
-   day4::part1(day4::TEST_INPUT);
-   day4::part1(day4::INPUT);
-   println!("\n=== DAY 4 - part 2 ===");
-   day4::part2(day4::TEST_INPUT);
-   day4::part2(day4::INPUT);
+   println!("\n=== DAY 5 - part 1 ===");
+   day5::part1(day5::TEST_INPUT);
+   day5::part1(day5::INPUT);
+   println!("\n=== DAY 5 - part 2 ===");
+   day5::part2(day5::TEST_INPUT);
+   day5::part2(day5::INPUT);
 }
